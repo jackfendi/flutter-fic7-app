5af27ec2845d38b36edd8acdd4da24a0699d0bdd
jackfendi <syahrulfendi44@gmail.com>
2023-12-21 18:26:35 +0700

    meet 11

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,3 @@
 class GlobalVariables {
-  static const baseUrl = 'https://f3de-2404-8000-1028-10fb-409a-90ff-3915-141.ngrok-free.app';
+  static const baseUrl = 'https://e91f-122-203-127-16.ngrok.io';
 }
